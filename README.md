639ce46c2b1cf1ff19b861861c9febae816426f1
mendhak <mendhak@users.noreply.github.com>
2018-01-24 22:12:55 +0000

    Writing values out to output.csv

---
+++ README.md
@@ -0,0 +1,4 @@
+
+
+A sample receiver for GPSLogger's Custom URL feature.  This application will receive the querystring parameters and write it to a local file. 
+
--- routes/index.js
+++ routes/index.js
@@ -4,20 +4,37 @@ var fs = require('fs');
 
 
 router.get('/', function(req, res, next) {
-
-  res.send("Use /log endpoint with any HTTP method ");
+    res.render('index')
 });
 
-router.all('/log', function(req, res, next){
+router.get('/log', function(req, res, next){
     var qsKeys = [];
+    var qsValues = [];
+
+    // Get your querystrings and values into arrays
     for(var q in req.query){
         qsKeys.push(q);
+        qsValues.push(req.query[q]);
     }
-    res.send(qsKeys.join(','));
 
-    var stream = fs.createWriteStream("output.txt", {flags:'a'});
-    stream.write(qsKeys.join(',') + "\n");
+
+    // output.csv header
+    var fileAlreadyExists = fs.existsSync("output.csv");
+    var stream = fs.createWriteStream("output.csv", {flags:'a'});
+    if (!fileAlreadyExists) {
+        stream.write(qsKeys.join(',') + "\n");
+    }
+
+
+    // write the values
+    stream.write(qsValues.join(',') + "\n");
     stream.end();
+
+    // For display purposes
+    res.set('Content-Type', 'text/plain');
+    res.write(qsKeys.join(',')+ "\n");
+    res.write(qsValues.join(',')+ "\n");
+    res.end();
 
 });
 
